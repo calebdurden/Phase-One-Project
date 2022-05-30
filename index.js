cdf2b47b4d1ef1306d6cbc5343ff2b015e13c2ff
Justin M. Ramon <justinmramon@gmail.com>
2022-05-30 18:28:51 -0500

    Random dog image, need button functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// is any of this shit working or nah?
+const randomDogImage = document.getElementById("randomDogImage");
+
+function getRandomImage(){
+  const randomImageApiUrl = "https://dog.ceo/api/breeds/image/random";
+
+  fetch(randomImageApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      randomDogImage.src = data.message;
+    })
+}
+
+getRandomImage();
