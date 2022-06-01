20b9796eafabb840334b02334ca134983fd6465e
Justin M. Ramon <justinmramon@gmail.com>
2022-05-31 20:47:19 -0500

    Dropdown list is functiong, need to work on filtering pictures tomorrow

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const randomDogImage = document.getElementById("randomDogImage");
+const randomDogButton = document.getElementById('random-btn');
+const dogBreedDropdown = document.getElementById('drop-btn');
 
 function getRandomImage(){
-  const randomImageApiUrl = "https://dog.ceo/api/breeds/image/random";
+  const randomDog = "https://dog.ceo/api/breeds/image/random";
 
-  fetch(randomImageApiUrl)
+  fetch(randomDog)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -11,4 +13,30 @@ function getRandomImage(){
     })
 }
 
-getRandomImage();
+function getBreedList(){
+    const dogBreeds = 'https://dog.ceo/api/breeds/list/all'
+
+    fetch(dogBreeds)
+    .then(response => response.json())
+    .then(data => {
+        //console.log("breeds", data);
+        dropDownList(data)
+    })
+}
+
+function dropDownList(breeds){
+  const breedListArray = Object.keys(breeds.message)
+
+  //console.log(breedListArray)
+  breedListArray.forEach(breed => {
+    let eachBreed = document.createElement('option')
+    eachBreed.text = breed
+
+    dogBreedDropdown.add(eachBreed)
+  })
+}
+
+getBreedList()
+
+randomDogButton.addEventListener('click', getRandomImage)
+dogBreedDropdown.addEventListener('onchange', getBreedList)
